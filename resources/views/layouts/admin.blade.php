6175858cd51885626a6891ff122e92916b8e9895
teddinata <teddinata@users.noreply.github.com>
2023-03-25 16:16:14 +0700

    http bug

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -6,7 +6,6 @@
       name="viewport"
       content="width=device-width, initial-scale=1, shrink-to-fit=no"
     />
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta name="description" content="" />
     <meta name="author" content="" />
      <!-- Favicon  -->

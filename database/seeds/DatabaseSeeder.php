3da1427cad46a86522d8e5729a95aa3656ec30bf
teddinata <teddinata@users.noreply.github.com>
2023-04-19 09:46:54 +0700

    migration seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -20,11 +20,11 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
 
         // IndoRegion
-        // $this->call(IndoRegionProvinceSeeder::class);
-        // $this->call(IndoRegionRegencySeeder::class);
-        // $this->call(IndoRegionDistrictSeeder::class);
-        // $this->call(IndoRegionVillageSeeder::class);
-        // $this->call(IndoRegionSeeder::class);
+        $this->call(IndoRegionProvinceSeeder::class);
+        $this->call(IndoRegionRegencySeeder::class);
+        $this->call(IndoRegionDistrictSeeder::class);
+        $this->call(IndoRegionVillageSeeder::class);
+        $this->call(IndoRegionSeeder::class);
 
     }
 }
